d9820d290509bce32e6364b608f94f47108de1da
Duncan McGreggor <oubiwann@gmail.com>
2017-12-23 10:40:32 -0600

    Re-added old testing profiles.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojusc/protobuf "3.4.0-2-SNAPSHOT"
-  :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
+  :description "A Clojure interface to Google's protocol buffers"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/clojusc/clojure-protobuf"
+  :url "https://github.com/clojusc/protobuf"
   :javac-options ["-target" "1.8" "-source" "1.8"]
   :java-source-paths ["src"]
   :lein-release {:deploy-via :clojars}
@@ -13,5 +13,8 @@
                  [org.flatland/io "0.3.0"]
                  [ordered-collections "0.4.2"]
                  [gloss "0.2.1"]]
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
+             :dev {:dependencies [[gloss "0.2.1"]]}}
   :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
   :checksum-deps true)
