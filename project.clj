203b25679e84f96db57d608a13867cb2be9f1e26
Duncan McGreggor <oubiwann@gmail.com>
2017-12-23 16:18:13 -0600

    Bumped for current release of protocol buffers.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/protobuf "3.4.0-v0.3"
+(defproject clojusc/protobuf "3.5.1-v0.3"
   :description "A Clojure interface to Google's protocol buffers"
   :url "https://github.com/clojusc/protobuf"
   :license {
