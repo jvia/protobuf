4684ffed0364b5efa7817bab2c10aeabb80177e6
Duncan McGreggor <oubiwann@gmail.com>
2017-12-23 12:55:02 -0600

    Got tests running again.

--- project.clj
+++ project.clj
@@ -1,22 +1,38 @@
 (defproject clojusc/protobuf "3.4.0-v0.3-SNAPSHOT"
   :description "A Clojure interface to Google's protocol buffers"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/clojusc/protobuf"
-  :javac-options ["-target" "1.8" "-source" "1.8"]
+  :license {
+    :name "Eclipse Public License"
+    :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :exclusions [
+    [org.clojure/clojure]]
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [com.google.protobuf/protobuf-java "3.4.0"]
+    [org.flatland/useful "0.11.5"]
+    [org.flatland/schematic "0.1.5"]
+    [org.flatland/io "0.3.0"]
+    [ordered-collections "0.4.2"]
+    [gloss "0.2.1"]]
   :java-source-paths ["src"]
-  :lein-release {:deploy-via :clojars}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.google.protobuf/protobuf-java "3.4.0"]
-                 [org.flatland/useful "0.11.5"]
-                 [org.flatland/schematic "0.1.5"]
-                 [org.flatland/io "0.3.0"]
-                 [ordered-collections "0.4.2"]
-                 [gloss "0.2.1"]]
-  :plugins [[lein-protobuf "0.5.0"]]
-  :protobuf-version "3.4.0"
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
-             :dev {:dependencies [[gloss "0.2.1"]]}}
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
-  :checksum-deps true)
+  :profiles {
+    :test {
+      :plugins [[lein-shell "0.5.0"]]
+      :java-source-paths ["target/test"]}
+    :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+    :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}}
+  :aliases {
+    "protoc-test" [
+       "with-profile"
+       "+test"
+       "shell"
+       "bin/compile-test-protobufs"]
+    "test-all" [
+      "with-profile"
+      "+1.3,+1.5,+default"
+      "test"]
+    "build-test" [
+      "do"
+        ["clean"]
+        ["protoc-test"]
+        ["test-all"]]})
