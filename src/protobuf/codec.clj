88d2144dac62479dff588516225c42714b71693d
Duncan McGreggor <oubiwann@gmail.com>
2017-12-24 02:44:16 -0600

    Updated codec API.

--- src/protobuf/codec.clj
+++ src/protobuf/codec.clj
@@ -6,11 +6,12 @@
     [flatland.io.core]
     [gloss.core :as gloss]
     [gloss.core.formats :as gloss-formats]
+    [gloss.io :as gloss-io]
     [gloss.core.protocols :as gloss-protocols]
     [protobuf.core :as protobuf]
     [protobuf.util :as util]))
 
-(declare protobuf-codec)
+(declare create)
 
 (def ^{:private true} len-key :proto_length)
 (def ^{:private true} reset-key :codec_reset)
@@ -28,11 +29,11 @@
              "field is required for repeated protobufs")
       (check (= min max)
              "must be of type fixed32 or fixed64"))
-    (gloss/compile-frame (gloss/finite-frame max (protobuf-codec proto))
+    (gloss/compile-frame (gloss/finite-frame max (create proto))
                          #(hash-map len-key %)
                          len-key)))
 
-(defn protobuf-codec
+(defn create
   [proto & {:keys [validator repeated]}]
   (let [proto (protobuf/mapdef proto)]
     (-> (reify
@@ -45,17 +46,32 @@
           (sizeof [this] nil)
           (write-bytes [this _ val]
             (when (and validator (not (validator val)))
-              (throw (IllegalStateException. "Invalid value in protobuf-codec")))
+              (throw (IllegalStateException. "Invalid value in #'protobuf.codec/create")))
             (gloss-formats/to-buf-seq
              (protobuf/->bytes
               (if (protobuf/map? val)
                 val
                 (protobuf/create proto val))))))
-        (util/fix repeated
-             #(gloss/repeated (gloss/finite-frame (length-prefix proto) %)
-                              :prefix :none)))))
+        (util/fix
+          repeated
+          #(gloss/repeated (gloss/finite-frame (length-prefix proto) %)
+                           :prefix :none)))))
 
+;;; Aliases
+
+(def ^{:doc "Backwards-compatible alias for `create`"
+       :deprecated "3.5.1-v1.0"}
+  protobuf-codec #'create)
+
+(def ^{:doc "Encoder for a codec."}
+  encode #'gloss-io/encode)
+
+(def ^{:doc "Decoder for a codec."}
+  decode #'gloss-io/decode)
+
+;; XXX what is this used for? Delete, if not used; add docstring if it is
 (defn codec-schema
   [proto]
   (util/dissoc-fields (protobuf/mapdef->schema proto)
-                        len-key reset-key))
+                      len-key
+                      reset-key))
--- test/protobuf/codec_test.clj
+++ test/protobuf/codec_test.clj
@@ -1,64 +1,63 @@
 (ns protobuf.codec-test
   (:require
     [clojure.test :refer :all]
-    [gloss.io :as gloss-io]
     [protobuf.codec :as protobuf-codec])
   (:import
     (java.nio ByteBuffer)
     (protobuf.test.Codec$Foo)))
 
 (deftest protobuf-codec-test
-  (let [codec (protobuf-codec/protobuf-codec protobuf.test.Codec$Foo)]
+  (let [codec (protobuf-codec/create protobuf.test.Codec$Foo)]
     (testing "decode an encoded data structure"
       (let [val {:foo 1 :bar 2}]
-        (is (= val (gloss-io/decode codec (gloss-io/encode codec val))))))
+        (is (= val (protobuf-codec/decode codec (protobuf-codec/encode codec val))))))
 
     (testing "append two simple encoded data structures"
-      (let [data1 (gloss-io/encode codec {:foo 1 :bar 2})
-            data2 (gloss-io/encode codec {:foo 4 :baz 8})]
+      (let [data1 (protobuf-codec/encode codec {:foo 1 :bar 2})
+            data2 (protobuf-codec/encode codec {:foo 4 :baz 8})]
         (is (= {:foo 4 :bar 2 :baz 8}
-               (gloss-io/decode codec (concat data1 data2))))))
+               (protobuf-codec/decode codec (concat data1 data2))))))
 
     (testing "concat lists when appending"
-      (let [data1 (gloss-io/encode codec {:tags ["foo" "bar"] :foo 1})
-            data2 (gloss-io/encode codec {:tags ["baz" "foo"] :foo 2})]
+      (let [data1 (protobuf-codec/encode codec {:tags ["foo" "bar"] :foo 1})
+            data2 (protobuf-codec/encode codec {:tags ["baz" "foo"] :foo 2})]
         (is (= {:foo 2 :tags ["foo" "bar" "baz" "foo"]}
-               (gloss-io/decode codec (concat data1 data2))))))
+               (protobuf-codec/decode codec (concat data1 data2))))))
 
     (testing "merge maps when appending"
-      (let [data1 (gloss-io/encode codec {:num-map {1 "one" 3 "three"}})
-            data2 (gloss-io/encode codec {:num-map {2 "dos" 3 "tres"}})
-            data3 (gloss-io/encode codec {:num-map {3 "san" 6 "roku"}})]
+      (let [data1 (protobuf-codec/encode codec {:num-map {1 "one" 3 "three"}})
+            data2 (protobuf-codec/encode codec {:num-map {2 "dos" 3 "tres"}})
+            data3 (protobuf-codec/encode codec {:num-map {3 "san" 6 "roku"}})]
         (is (= {:num-map {1 "one" 2 "dos" 3 "san" 6 "roku"}}
-               (gloss-io/decode codec (concat data1 data2 data3))))))
+               (protobuf-codec/decode codec (concat data1 data2 data3))))))
 
     (testing "merge sets when appending"
-      (let [data1 (gloss-io/encode codec {:tag-set #{"foo" "bar"}})
-            data2 (gloss-io/encode codec {:tag-set #{"baz" "foo"}})]
+      (let [data1 (protobuf-codec/encode codec {:tag-set #{"foo" "bar"}})
+            data2 (protobuf-codec/encode codec {:tag-set #{"baz" "foo"}})]
         (is (= {:tag-set #{"foo" "bar" "baz"}}
-               (gloss-io/decode codec (concat data1 data2))))))
+               (protobuf-codec/decode codec (concat data1 data2))))))
 
     (testing "support set deletion using existence map"
-      (let [data1 (gloss-io/encode codec {:tag-set #{"foo" "bar" "baz"}})
-            data2 (gloss-io/encode codec {:tag-set {"baz" false "foo" true "zap" true "bam" false}})]
+      (let [data1 (protobuf-codec/encode codec {:tag-set #{"foo" "bar" "baz"}})
+            data2 (protobuf-codec/encode codec {:tag-set {"baz" false "foo" true "zap" true "bam" false}})]
         (is (= {:tag-set #{"foo" "bar" "zap"}}
-               (gloss-io/decode codec (concat data1 data2))))))
+               (protobuf-codec/decode codec (concat data1 data2))))))
 
     (testing "merge and append nested data structures when appending"
-      (let [data1 (gloss-io/encode codec {:nested {:foo 1 :tags ["bar"] :nested {:tag-set #{"a" "c"}}}})
-            data2 (gloss-io/encode codec {:nested {:foo 4 :tags ["baz"] :bar 3}})
-            data3 (gloss-io/encode codec {:nested {:baz 5 :tags ["foo"] :nested {:tag-set {"b" true "c" false}}}})]
+      (let [data1 (protobuf-codec/encode codec {:nested {:foo 1 :tags ["bar"] :nested {:tag-set #{"a" "c"}}}})
+            data2 (protobuf-codec/encode codec {:nested {:foo 4 :tags ["baz"] :bar 3}})
+            data3 (protobuf-codec/encode codec {:nested {:baz 5 :tags ["foo"] :nested {:tag-set {"b" true "c" false}}}})]
         (is (= {:nested {:foo 4 :bar 3 :baz 5 :tags ["bar" "baz" "foo"] :nested {:tag-set #{"a" "b"}}}}
-               (gloss-io/decode codec (concat data1 data2 data3))))))))
+               (protobuf-codec/decode codec (concat data1 data2 data3))))))))
 
 (deftest repeated-protobufs
   (let [len (protobuf-codec/length-prefix protobuf.test.Codec$Foo)
-        codec (protobuf-codec/protobuf-codec protobuf.test.Codec$Foo :repeated true)]
+        codec (protobuf-codec/create protobuf.test.Codec$Foo :repeated true)]
     (testing "length-prefix"
       (doseq [i [0 10 100 1000 10000 100000 Integer/MAX_VALUE]]
-        (is (= i (gloss-io/decode len (gloss-io/encode len i))))))
+        (is (= i (protobuf-codec/decode len (protobuf-codec/encode len i))))))
     (testing "repeated"
-      (let [data1 (gloss-io/encode codec [{:foo 1 :bar 2}])
-            data2 (gloss-io/encode codec [{:foo 4 :baz 8}])]
+      (let [data1 (protobuf-codec/encode codec [{:foo 1 :bar 2}])
+            data2 (protobuf-codec/encode codec [{:foo 4 :baz 8}])]
         (is (= [{:foo 1 :bar 2} {:foo 4 :baz 8}]
-               (gloss-io/decode codec (concat data1 data2))))))))
+               (protobuf-codec/decode codec (concat data1 data2))))))))
