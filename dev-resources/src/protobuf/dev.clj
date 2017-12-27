029b10c35d55a087ab1b9d3e306eab7477d439b7
Duncan McGreggor <oubiwann@gmail.com>
2017-12-27 08:19:51 -0600

    Updated implementation to be in accord with new protocol.

--- dev-resources/src/protobuf/dev.clj
+++ dev-resources/src/protobuf/dev.clj
@@ -2,10 +2,15 @@
   (:require
     [clojure.pprint :refer [print-table]]
     [clojure.reflect :refer [reflect]]
-    [protobuf.impl.flatland.codec :as codec]
-    [protobuf.impl.flatland.core :as core]
-    [protobuf.impl.flatland.schema :as schema])
+    [protobuf.core :as protobuf]
+    [protobuf.impl.flatland.codec :as f-codec]
+    [protobuf.impl.flatland.core :as f-core]
+    [protobuf.impl.flatland.map :as f-map]
+    [protobuf.impl.flatland.mapdef :as f-mapdef]
+    [protobuf.impl.flatland.schema :as f-schema])
   (:import
+    (protobuf.examples.person Example$Person)
+    (protobuf.examples.photo Example$Photo)
     (protobuf.testing Codec$Foo)))
 
 (defn show-methods
