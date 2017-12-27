cd016a09aaf07860d99acb787e0e2b38f99abe47
Duncan McGreggor <oubiwann@gmail.com>
2017-12-27 17:11:33 -0600

    Massive refactor on API and impl.

---
+++ src/clj/protobuf/common.clj
@@ -0,0 +1,52 @@
+(ns protobuf.common)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn get-class
+  [this]
+  (:protobuf-class (.contents this)))
+
+(defn get-instance
+  [this]
+  (:instance (.contents this)))
+
+(defn get-wrapper
+  [this]
+  (:java-wrapper (.contents this)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Behaviours   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def associative-behaviour
+  {:containsKey (fn [this data] (.containsKey (get-instance this) data))
+   :entryAt (fn [this data] (.entryAt (get-instance this) data))})
+
+(def iterable-behaviour
+  {:forEach (fn [this consumer] (.forEach (get-instance this) consumer))
+   :iterator (fn [this] (.iterator (get-instance this)))
+   :spliterator (fn [this] (.spliterator (get-instance this)))})
+
+(def lookup-behaviour
+  {:valAt (fn ([this k] (.valAt (get-instance this) k))
+              ([this k fallback] (.valAt (get-instance this) k fallback)))})
+
+(def persistent-collection-behaviour
+  {:cons (fn [this o] (.cons (get-instance this) o))
+   :count (fn [this] (.count (get-instance this)))
+   :empty (fn [this] (.empty (get-instance this)))
+   :equiv (fn [this o]
+           (and (= (get-class this) (get-class o))
+                (.equiv (get-instance this) (get-instance o))))})
+
+(def persistent-map-behaviour
+  {:assoc (fn [this k v] (.assoc (get-instance this) k v))
+   :without (fn [this k] (.without (get-instance this) k))})
+
+(def printable-behaviour
+  {:toString (fn [this] (.toString (get-instance this)))})
+
+(def seqable-behaviour
+  {:seq (fn [this] (.seq (get-instance this)))})
--- src/clj/protobuf/impl/flatland/core.clj
+++ src/clj/protobuf/impl/flatland/core.clj
@@ -1,40 +1,119 @@
 (ns protobuf.impl.flatland.core
   "This implementation takes its name from the original code for this project
   that was done under the flatland Github org and which pulled in several
-  flatland libraries as dependencies."
+  flatland libraries as dependencies.
+
+  This namespace is an internal implementation detail not intended for
+  developers. The API for working with all implementations of protocol
+  buffer backends for this project is provided in the `protobuf.core`
+  namespace."
   (:require
+    [protobuf.common :as common]
     [protobuf.impl.flatland.map :as protobuf-map]
-    [protobuf.impl.flatland.mapdef :as protobuf]))
+    [protobuf.impl.flatland.mapdef :as protobuf])
+  (:gen-class
+    :name protobuf.impl.flatland.core.FlatlandProtoBuf
+    :implements [clojure.lang.Associative
+                 clojure.lang.ILookup
+                 clojure.lang.IPersistentCollection
+                 clojure.lang.IPersistentMap
+                 clojure.lang.Seqable
+                 java.lang.Iterable]
+    :init init
+    :constructors {[java.lang.Class
+                    clojure.lang.APersistentMap]
+                   []}
+    :methods [^:static [schema [Object] Object]]
+    :state contents
+    :main false))
 
-(defrecord FlatlandProtoBuf
-  [protobuf-class
-   java-wrapper
-   init-data
-   instance])
-
-(defn create
+(defn -init
   [protobuf-class data]
   (let [wrapper (protobuf/mapdef protobuf-class)]
-    (map->FlatlandProtoBuf
-      {:protobuf-class protobuf-class
-       :java-wrapper wrapper
-       :instance (protobuf/create wrapper data)})))
+    [[] {:instance (protobuf/create wrapper data)
+         :java-wrapper wrapper
+         :protobuf-class protobuf-class}]))
 
-(defn schema
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   clojure.lang.Associative   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def -containsKey (:containsKey common/associative-behaviour))
+(def -entryAt (:entryAt common/associative-behaviour))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   clojure.lang.ILookup   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def -valAt (:valAt common/lookup-behaviour))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   clojure.lang.IPersistentCollection   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def -cons (:cons common/persistent-collection-behaviour))
+(def -count (:count common/persistent-collection-behaviour))
+(def -empty (:empty common/persistent-collection-behaviour))
+(def -equiv (:equiv common/persistent-collection-behaviour))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   clojure.lang.IPersistentMap   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn -assoc
+  [this k v]
+  (new protobuf.impl.flatland.core.FlatlandProtoBuf
+    (common/get-class this)
+    ((:assoc common/persistent-map-behaviour) this k v)))
+
+(defn -without
+  [this k]
+  (new protobuf.impl.flatland.core.FlatlandProtoBuf
+    (common/get-class this)
+    ((:without common/persistent-map-behaviour) this k)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   clojure.lang.Seqable   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def -seq (:seq common/seqable-behaviour))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   java.lang.Iterable   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def -forEach (:forEach common/iterable-behaviour))
+(def -iterator (:iterator common/iterable-behaviour))
+(def -spliterator (:spliterator common/iterable-behaviour))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   java.lang.Object   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def -toString (:toString common/printable-behaviour))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   protobuf.core.ProtoBufAPI   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn -schema
   [protobuf-class]
   (protobuf/mapdef->schema
    (protobuf/mapdef protobuf-class)))
 
 (def behaviour
   {:->bytes (fn [this]
-             (protobuf-map/->bytes (:instance this)))
-   :->schema (fn [this & args]
-              (apply protobuf/mapdef->schema args))
+             (protobuf-map/->bytes (common/get-instance this)))
+   :->schema (fn [this]
+             (protobuf/mapdef->schema (common/get-wrapper this)))
    :bytes-> (fn [this bytes]
-             (create
-              (:protobuf-class this)
-              (protobuf/parse (:java-wrapper this) bytes)))
+             (new protobuf.impl.flatland.core.FlatlandProtoBuf
+               (common/get-class this)
+               (protobuf/parse (common/get-wrapper this) bytes)))
    :read (fn [this in]
-          (protobuf/read (:java-wrapper this) in))
-   :write (fn [this out & protobufs]
-           (apply protobuf/write (concat [out] protobufs)))})
+          (new protobuf.impl.flatland.core.FlatlandProtoBuf
+            (common/get-class this)
+            (first
+              (protobuf/read (common/get-wrapper this) in))))
+   :write (fn [this out]
+           (protobuf/write out (common/get-instance this)))})
