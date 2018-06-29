3840d3d81ce9a3e367571e222220f92912db37ec
Jeremiah Via <jeremiah.via@nytimes.com>
2018-06-28 20:43:29 -0600

    [#25] Add failing test case

--- test/protobuf/examples/photo_test.clj
+++ test/protobuf/examples/photo_test.clj
@@ -1,26 +1,25 @@
 (ns protobuf.examples.photo-test
   (:require
-    [clojure.test :refer :all]
-    [protobuf.core :as protobuf])
+   [clojure.test :refer :all]
+   [protobuf.core :as protobuf])
   (:import
-    (com.google.protobuf ByteString)
-    (protobuf.examples.photo Example$Photo)))
+   (com.google.protobuf ByteString)
+   (protobuf.examples.photo Example$Photo)))
 
-(def data {
-  :id 7
-  :path "/photos/h2k3j4h9h23"
-  :labels #{"hawaii" "family" "surfing"},
-  :attrs {"color space" "RGB" "dimensions" "1632x1224" "alpha" "no"},
-  :tags {
-    4 {
-      :person-id 4
-      :x-coord 607
-      :y-coord 813
-      :width 25
-      :height 27}}
-  :image (ByteString/copyFrom
-          (byte-array
-           (map unchecked-byte [1 2 3 4 -1])))})
+(def data
+  {:id 7
+   :path "/photos/h2k3j4h9h23"
+   :labels #{"hawaii" "family" "surfing"},
+   :attrs {"color space" "RGB" "dimensions" "1632x1224" "alpha" "no"},
+   :tags {4 {:person-id 4
+             :x-coord 607
+             :y-coord 813
+             :width 25
+             :height 27}}
+   :type :png
+   :image (ByteString/copyFrom
+           (byte-array
+            (map unchecked-byte [1 2 3 4 -1])))})
 
 (deftest example-as-map-test
   (is (= data
